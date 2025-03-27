264b2ab9c9258bb53b3fbb0e554d7447d2ca113b
Carl Hamer <carl.hamer2848@gmail.com>
2025-03-27 13:20:12 -0400

    Modified the print statement to fit the nuber of sides on the die.

--- src/D12.py
+++ src/D12.py
@@ -3,7 +3,7 @@ from Die import D12
 def main():
     d12 = D12()
     roll_result = d12.roll()
-    print(f"You rolled a 6-sided die and got: {roll_result}")
+    print(f"You rolled a 12-sided die and got: {roll_result}")
 
 if __name__ == "__main__":
     main()
