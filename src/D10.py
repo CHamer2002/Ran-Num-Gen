9e7f59472ef3d54a7a82de66b062d22018ec462c
Carl Hamer <carl.hamer2848@gmail.com>
2025-03-27 13:10:55 -0400

    Added the D810 class as well as the D10 parameter to Die

---
+++ src/D10.py
@@ -0,0 +1,9 @@
+from Die import D10
+
+def main():
+    d10 = D10()
+    roll_result = d10.roll()
+    print(f"You rolled a 6-sided die and got: {roll_result}")
+
+if __name__ == "__main__":
+    main()
